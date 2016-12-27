cbe7e2088dbb278d225f3dceb6a6c4b6e965a7a5
Lunar <lunar@cypherpunk.is>
2016-12-27 05:33:48 -0600

    Create speedtest.sh

---
+++ speedtest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ipv4 () {
+ipiv=$( wget -qO- ipv4.icanhazip.com )
+echo "Your public IPv4 is $ipiv"
+#GigeNET | Chicago, IL | http://chi.gigenet.com.speedtest.net/
+wget -4 -O /dev/null http://speedtest.chi.gigenet.com/1gb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}'
+#GigeNET | Los Angeles, CA | http://lax.gigenet.com.speedtest.net/
+wget -4 -O /dev/null http://speedtest.lax.gigenet.com/1gb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}'
+#GigeNET | Washington, DC | http://iad.gigenet.com.speedtest.net/
+wget -4 -O /dev/null http://speedtest.iad.gigenet.com/1gb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}'
+}
