8f983645fa742bf77fce3a71e9b0e47c0216d5ce
Lunar <lunar@cypherpunk.is>
2016-12-27 05:57:15 -0600

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 ip=$( wget -qO- ipv4.icanhazip.com )
 echo "Your public IPv4 is $ip"
+echo ""
 echo "GigeNET | Chicago, IL | http://chi.lg.gigenet.com/"
 chi=$( wget -O /dev/null http://speedtest.chi.gigenet.com/100mb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "$chi"
+echo "Download speed: $chi"
+echo ""
 echo "GigeNET | Los Angeles, CA | http://lax.lg.gigenet.com/"
 lax=$( wget -O /dev/null http://speedtest.lax.gigenet.com/100mb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "$lax"
+echo "Download speed: $lax"
+echo ""
 echo "GigeNET | Washington, DC | http://iad.lg.gigenet.com/"
 iad=$( wget -O /dev/null http://speedtest.iad.gigenet.com/100mb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "$iad"
+echo "Download speed: $iad"
