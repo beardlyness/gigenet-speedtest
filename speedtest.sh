bb7669b1b9d782dbbb251faa9d9a3a6d1accc408
Lunar <lunar@cypherpunk.is>
2016-12-27 05:51:43 -0600

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -2,11 +2,11 @@
 ip=$( wget -qO- ipv4.icanhazip.com )
 echo "Your public IPv4 is $ip"
 echo "GigeNET | Chicago, IL | http://chi.lg.gigenet.com/"
-chi=$( wget -O /dev/null http://speedtest.chi.gigenet.com/1gb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
+chi=$( wget -O /dev/null http://speedtest.chi.gigenet.com/100mb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
 echo "$chi"
 echo "GigeNET | Los Angeles, CA | http://lax.lg.gigenet.com/"
-lax=$( wget -O /dev/null http://speedtest.lax.gigenet.com/1gb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
+lax=$( wget -O /dev/null http://speedtest.lax.gigenet.com/100mb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
 echo "$lax"
 echo "GigeNET | Washington, DC | http://iad.lg.gigenet.com/"
-iad=$( wget -O /dev/null http://speedtest.iad.gigenet.com/1gb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
+iad=$( wget -O /dev/null http://speedtest.iad.gigenet.com/100mb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
 echo "$iad"
