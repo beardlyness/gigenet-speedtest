a02a357262f9f571a37c01a069db80e13fbb97cb
Lunar <lunar@cypherpunk.is>
2016-12-28 09:44:54 -0600

    100MB test files

--- speedtest.sh
+++ speedtest.sh
@@ -8,18 +8,18 @@ echo ""
 echo "--------------------------------------------------"
 echo "GigeNET | Chicago, IL | http://chi.lg.gigenet.com/"
 echo "--------------------------------------------------"
-chi=$( wget -O /dev/null http://speedtest.chi.gigenet.com/1gb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
+chi=$( wget -O /dev/null http://speedtest.chi.gigenet.com/100mb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
 echo "Download Speed: $chi"
 echo ""
 echo "------------------------------------------------------"
 echo "GigeNET | Los Angeles, CA | http://lax.lg.gigenet.com/"
 echo "------------------------------------------------------"
-lax=$( wget -O /dev/null http://speedtest.lax.gigenet.com/1gb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
+lax=$( wget -O /dev/null http://speedtest.lax.gigenet.com/100mb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
 echo "Download Speed: $lax"
 echo ""
 echo "-----------------------------------------------------"
 echo "GigeNET | Washington, DC | http://iad.lg.gigenet.com/"
 echo "-----------------------------------------------------"
-iad=$( wget -O /dev/null http://speedtest.iad.gigenet.com/1gb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
+iad=$( wget -O /dev/null http://speedtest.iad.gigenet.com/100mb.img 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
 echo "Download Speed: $iad"
 echo ""
